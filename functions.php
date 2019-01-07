fcfa3204e7e4c3dee07a7ecddc04852f44021546
Alvaro Separovich <alvasepro@gmail.com>
2019-01-07 14:35:03 -0200

    adicionado botões na pagina do produto para aumentar e diminuir quantidade

--- functions.php
+++ functions.php
@@ -76,6 +76,24 @@ function wc_elshaddai_ordernote( $fields ) {
      return $fields;}
 add_filter( 'woocommerce_checkout_fields' , 'wc_elshaddai_ordernote' );
 
+function add_cart_buttons(){
+  echo('
+  <button class="bt123+" type="button" onclick="addItem(); return false;">+</button>
+  <button class="bt123-" type="button" onclick="removeItem(); return false;">-</button>
+  <script> 
+  function removeItem(){
+    e = document.querySelector("form.cart > .quantity > input.input-text");n = e.value;newN = n-1;
+      if (!(newN < e.min)){ e.value = parseInt(n)-1; }}
+  
+  function addItem(){
+    e = document.querySelector("form.cart > .quantity > input.input-text");n = e.value;newN = n+1;
+      if (!(newN > e.max)){ e.value = parseInt(n)+1; }}    
+  
+  </script>
+  '
+);
+}
+add_filter('woocommerce_after_add_to_cart_quantity','add_cart_buttons', 0);
 
 function retrieve_var1_replacement( $especial_attribute=0, $all=0 ) {
   //only run on products
