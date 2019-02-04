5cedbaacfb5e54b722e57ccdd670d367f48bd4d5
Alvaro Separovich <alvasepro@gmail.com>
2019-02-04 13:46:11 -0200

    minimal Identation issue fixed

--- functions.php
+++ functions.php
@@ -33,24 +33,24 @@ function custom_dashboard_help() {
 }
 
 if ( ! function_exists( 'estore_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function estore_1setup() {
-   // Cropping the images to different sizes to be used in the theme
-	add_image_size( 'estore-medium-image111', 240, 240);
-	add_image_size( 'elshaGrid1', 110, 120);
+  /**
+   * Sets up theme defaults and registers support for various WordPress features.
+   *
+   * Note that this function is hooked into the after_setup_theme hook, which
+   * runs before the init hook. The init hook is too late for some features, such
+   * as indicating support for post thumbnails.
+   */
+  function estore_1setup() {
+    // Cropping the images to different sizes to be used in the theme
+    add_image_size( 'estore-medium-image111', 240, 240);
+    add_image_size( 'elshaGrid1', 110, 120);
 
-  // Cropping the images to different sizes to be used in the theme **************REMOVE*****************
- remove_image_size( 'estore-featured-image');
- remove_image_size( 'estore-product-grid');
- remove_image_size( 'estore-square');
- remove_image_size( 'estore-slider');
-}
+    // Cropping the images to different sizes to be used in the theme **************REMOVE*****************
+  remove_image_size( 'estore-featured-image');
+  remove_image_size( 'estore-product-grid');
+  remove_image_size( 'estore-square');
+  remove_image_size( 'estore-slider');
+  }
 endif; // estore_setup
 add_action( 'after_setup_theme', 'estore_1setup', 11 );
 
@@ -104,7 +104,7 @@ function add_cart_button_plus(){
       echo'<button class="btElPlus" type="button" onclick="addItem(); return false;">+</button>';
       }
     }
-
+  
   
 function add_buttons_js_snippet(){echo'<script>
   function removeItem(){	e = document.querySelector("form.cart .quantity > input.input-text");	n = e.value;newN = parseInt(n)-1;  if (!(newN < e.min)){ e.value = newN; }}
