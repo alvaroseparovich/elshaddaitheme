da1ee4d348a02f52ec5c8fbe3f2ad62bcc26da96
Alvaro Separovich <alvasepro@gmail.com>
2019-02-28 11:11:10 -0300

    scss file modified and css renamed

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.0.2.5.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style-sheet.css' );
     wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js-el-shaddai.min.js' );
 }
 /* Se qualquer mudança for feita no Css, a versão do thema deve ser atualizada para que nenhum
