2c80703005ec89ca0cad4acd16b3e70ba8ffddd7
Alvaro Separovich <alvasepro@gmail.com>
2019-09-30 14:21:30 -0300

    -Impov: Text of the Searchform

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
  */
 ?>
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'estore' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+	<input type="search" class="search-field" placeholder="Procurar Livro / Autor" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	<button type="submit" class="searchsubmit" name="post_type" value="product"><i class="fa fa-search"></i></button>
 </form>
