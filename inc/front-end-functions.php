f7873e22ec0031a82b2f7701991203ecde03d7de
Alvaro Separovich <alvasepro@gmail.com>
2019-11-08 16:08:40 -0300

    Tweak: Header of Single Post modified

--- inc/front-end-functions.php
+++ inc/front-end-functions.php
@@ -10,3 +10,20 @@ function elshaddai_sale_flash($awe,$qwe,$product ){
     return;
 }
 add_filter( 'woocommerce_sale_flash', 'elshaddai_sale_flash', 10,3 );
+
+function estore_entry_title() {
+    if (is_singular('post')):
+        the_title( '<h1 class="entry-title"  style="margin: 0.4em 0 1em;">', '</h1>' );
+    elseif ( is_singular() ) :
+        the_title( '<h1 class="entry-title">', '</h1>' );
+    elseif ( is_archive() ) :
+        the_archive_title( '<h1 class="page-title">', '</h1>' );
+    elseif ( is_404() ) :
+        ?>
+        <h1 class="page-title"> <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'estore' ); ?></h1>
+    <?php
+    else :
+        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+    endif;
+
+}
