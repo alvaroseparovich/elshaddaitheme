f0052741c5dbb7459dc468593a4647fe32780375
Alvaro Separovich <alvasepro@gmail.com>
2020-11-08 21:55:02 -0300

    FEAT: Fixed Head | sticky header

--- static/index.php
+++ static/index.php
@@ -5,6 +5,7 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/static/css/style-sheet.css' );
     wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js-el-shaddai.min.js' );
+    wp_enqueue_script( 'script-sticky-header', get_stylesheet_directory_uri() . '/static/js/sticky-header.js' );
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
